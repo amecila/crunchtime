2d13ebd51544a40c9580ef0d63be37679d1a4d7a
Alice Ma <alicetyma@gmail.com>
2014-11-01 05:15:48 -0400

    controller started!

--- app/app.js
+++ app/app.js
@@ -1,4 +1,28 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('crunchtimeApp', ['ngMaterial']);
+var crunchtimeApp = angular.module('crunchtimeApp', ['ngMaterial']);
+
+//Controllers
+crunchtimeApp.controller('crunchtimeAppCtrl', function($scope) {
+    $scope.todos = [
+        'Find a bunny',
+        'Pet the bunny',
+        'Repeat'
+    ];
+
+    $scope.done = function(todo) {
+        var indexOf = $scope.todos.indexOf(todo);
+        if (indexOf !== -1) { //only do something if task is inside the list
+            $scope.todos.splice (indexOf, 1);
+        }
+    }
+
+    $scope.add = function(e) {
+        if (e.which && e.which === 13) { //13 is the enter button
+            $scope.todos.push($scope.newTodo);
+            $scope.newTodo = '';
+        }
+    }
+
+});
