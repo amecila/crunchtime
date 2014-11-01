1c78b34adad6a679a1fa422ffb309b956aa00c28
Alice Ma <alicetyma@gmail.com>
2014-11-01 15:28:29 -0400

    Progress bar works now. Theme-ing should work, but there's an upstream issue.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,10 @@
 var crunchtimeControllers = angular.module("crunchtimeControllers", []);
 
 crunchtimeControllers.controller('crunchtimeAppCtrl', function($scope, $interval) {
-  $scope.todos = [{description: "something very very long", startTime: new Date(), endTime: new Date(2014, 11, 30)},
-    {description: 'short', startTime: new Date(), endTime: new Date(2014, 11, 30)}
+  $scope.todos = [
+    {description: "something very very long", startTime: new Date(), endTime: new Date(2014, 11, 30)},
+    {description: 'short', startTime: new Date(), endTime: new Date(2014, 11, 30)},
+    {description: 'almost done', startTime: new Date(2014, 9, 30), endTime: new Date(2014, 10, 2)}
   ];
 
   var today = new Date();
@@ -40,10 +42,19 @@ crunchtimeControllers.controller('crunchtimeAppCtrl', function($scope, $interval
         todo.countdown = ((todo.endTime - now) / (1000 * 60 * 60)).toFixed(5) + " hours left.";
       }
       else {
-
         todo.countdown = "OVERDUE";
       }
     }
+
+    for (var i = 0; i < $scope.todos.length; i++) {
+      var todo = $scope.todos[i];
+      todo.percentageCompleted = (now - todo.startTime) / (todo.endTime - todo.startTime) * 100;
+      if (todo.percentageCompleted < 33) todo.theme = 'green';
+      else if (todo.percentageCompleted < 67) todo.theme = 'yellow';
+      else if (todo.percentageCompleted <= 100) todo.theme = 'orange';
+      else todo.theme = 'red';
+    }
+
   }
 
   $interval(updateCountdown, 30);
